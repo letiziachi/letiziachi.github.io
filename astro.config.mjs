44fff1ac8ce0bb8c50cf32c43bdfa68d8391c200
diesys <turingsbite@gmail.com>
2024-02-24 23:22:47 +0100

    add scroll animation fade in

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astronaut.github.io',
+  output: 'hybrid',
   integrations: [mdx(), sitemap(), tailwind()],
   i18n: {
     defaultLocale: "it",
