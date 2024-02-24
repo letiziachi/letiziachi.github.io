a1eaa70b06fab50c44e648c0395abd263ac1f3f1
Letizia Chianello <bonkiettina@gmail.com>
2024-02-24 17:58:29 +0100

    Aggiunto script deploy dockerizzato
    Utile ovunque, necessario su macchine con versioni vecchie di node

---
+++ dev_deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run \
+        --rm --interactive --tty \
+        --network host \
+        --volume "$PWD":"$PWD" \
+        --workdir "$PWD" \
+        node:19-bullseye \
+	/bin/bash -c "npm install && npm run dev"
+
