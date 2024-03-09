00fea305c58e2437a236678d2b502c837db9206e
Letizia Chianello <bonkiettina@gmail.com>
2024-03-09 17:59:14 +0100

    Inserito testo Progeto La pampa di Tagghjavia

--- src/lib/projects/La_pampa_di_Tagghjavia.sh
+++ src/lib/projects/La_pampa_di_Tagghjavia.sh
@@ -45,6 +45,28 @@ ENDLIST
 function projectText()
 {
 	cat << ENDTEXT
+Ci troviamo sull'ultima valle a ovest dei Monti Nebrodi, da qui possiamo specchiarci con le Madonie.  
+Osservo Vittorio e Sibilla che scendono lungo la campagna e giocano saltando sulle rocce.  
+In lontananza, vedono una mandria di pecore e decidono di seguirla per un po', per poi osservarla dall'alto di un masso.  
+
+Scendendo ancora, troviamo della lana abbandonata fra i sassi. Con meraviglia, i bambini la toccano, la annusano,  
+la osservano, la passano da una mano all'altra. Anche Gio ne prende un pezzo e, con delicatezza, inizia ad allungare  
+la lana e a rotolarla fra le dita. I bambini ripetono gli stessi gesti e formano insieme a lui dei bracciali caldi  
+e soffici, simbolo prezioso dell'esplorazione.  
+
+Continuiamo a scendere e, con grande capacità di astrazione, Vittorio e Sibilla iniziano a correre gridando *La pampa está aquí*...  
+
+-  
+
+*"L'educazione dei sensi è la via maestra per potenziare le capacità mentali"*  
+
+*"Il bambino ha bisogno di vivere naturalmente, di “vivere” la natura e non soltanto di conoscerla, studiandola o ammirandola"*  
+
+*"Il fatto più importante risiede proprio nel liberare possibilmente il fanciullo dai legami che lo isolano nella vita artificiale*  
+creata dalla convivenza cittadina”
+
+
+Maria Montessori
 ENDTEXT
 }
 
