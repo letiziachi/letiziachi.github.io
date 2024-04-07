948fc6d3b00d5b0cfd9ce60728f70fbd80c3a453
flowin.space <turingsbite@gmail.com>
2024-04-07 22:11:17 +0200

    completed spanish functionality and some translations and migrations

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,7 @@ import { z, defineCollection } from "astro:content";
 
 const projectSchema = ({ image }) => z.object({
     title: z.string(),
+    title_es: z.string(),
     custom_link_label: z.string().optional(),
     custom_link: z.string().optional(),
     description: z.string().optional(),
